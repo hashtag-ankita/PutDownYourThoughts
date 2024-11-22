c670a6aafbac108baf1dbbce077deb76aca80ece
Ankita Dey <138559097+hashtag-ankita@users.noreply.github.com>
2024-11-23 00:32:52 +0530

    Added few template files
    1. Added main.html
    
    2. Added navbar.html
    
    3. Added home.html
    
    4. Added create_post.html (but have not wrote content in it yet)
    
    Added content for feed section in home.html | user name is left to be added in model and template | added some minimal styling as well

--- base/views.py
+++ base/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from .models import Post, Category, Tag
 
 # Create your views here.
+def home(request):
+    """
+    Render the home page.
+
+    Args:
+        request: The HTTP request object.
+
+    Returns:
+        HttpResponse object with the rendered home page.
+    """
+
+    posts = Post.objects.all()
+    categories = Category.objects.all()
+    tags = Tag.objects.all()
+
+
+    context = {
+        'posts': posts,
+        'categories': categories,
+        'tags': tags,
+        }
+    return render(request, 'home.html', context)
+
+def createPost(request):
+    return render(request, 'create_post.html')
