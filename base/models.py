45387a62b5c88499765ac55045245d13fb796a98
Ankita Dey <138559097+hashtag-ankita@users.noreply.github.com>
2024-11-25 14:07:39 +0530

    Separated feed_component.html & sidebar_component.html from home.html for modularity
    Changed the overall color palette as well (in home.html & main.html)

--- base/models.py
+++ base/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -18,7 +19,7 @@ class Tag(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=100)
     '''Add author field when adding author model and authentication feature'''
-    # author = models.ForeignKey(User, on_delete=models.CASCADE)  # Uncomment and use User model when ready.
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # Uncomment and use User model when ready.
     content = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     tags = models.ManyToManyField(Tag, blank=True)  # Allow blank for posts without tags.
